2d8f11a84fea133a7d3e24354b49154f0ea1d0b6
Никита Мокляк <nikita.dezertir@gmail.com>
2023-02-18 18:04:24 +0400

    init

--- PlatformServer/src/main/java/com/moklyak/platformserver/service/ClientService.java
+++ PlatformServer/src/main/java/com/moklyak/platformserver/service/ClientService.java
@@ -2,18 +2,13 @@ package com.moklyak.platformserver.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.ConcurrentModificationException;
+
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
